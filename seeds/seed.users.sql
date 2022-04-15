9de873936bae89ba0ee57d607a04ef33636895b4
Brendan Loomis <brendanloomis@gmail.com>
2022-04-15 16:51:37 -0700

    Add migrations and seeds

---
+++ seeds/seed.users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users
+    (first_name, last_name, username, password)
+    VALUES
+        ('Demo', 'Account', 'demo', 'mypass123'),
+        ('Second', 'Test', 'demo2', 'demopass45');
---
+++ seeds/seed.wines.sql
@@ -0,0 +1,12 @@
+INSERT INTO wines
+    (wine_name, winery, varietal, vintage, rating, notes, user_id)
+    VALUES
+    ('Justin Cabernet', 'Justin', 'Cabernet Sauvignon', '2014', '5', 'Great cab!'),
+    ('Rombauer Chardonnay', 'Rombauer', 'Chardonnay', '2015', '4', 'My favorite'),
+    ('Veuve Clicquot Yellow Label Champagne Brut', 'Veuve Clicquot', 'Sparkling', null, 'Great champagne'),
+    ('Machete', 'Orin Swift', 'Red Blend', '2019', '5', 'Best red blend!'),
+    ('Kim Crawford Sauvignon Blanc', 'Kim Crawford', 'Sauvignon Blanc', '2020', '4', 'Great sauv blanc'),
+    ('Apothic Red', 'Apothic', 'Red Blenc', null, '3', 'Good for the price'),
+    ('The Prisoner Red Blend', 'The Prisoner Wine Company', 'Red Blend', '2019', '5', 'One of my favorites'),
+    ('La Marca Prosecco', 'La Marca', 'Sparkling', null, '4', 'Great sparkling for the price'),
+    ('Cote des Roses Rosé', 'Gérard Bertrand', 'Rosé', null, '4', 'Great rosé for the price');
