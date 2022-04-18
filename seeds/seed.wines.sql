a268cd97870c956a7ba36b953125caea1e35b376
Brendan Loomis <brendanloomis@gmail.com>
2022-04-18 15:02:42 -0700

    Set up postgrator and scripts to migrate, fix wines seeds

--- seeds/seed.wines.sql
+++ seeds/seed.wines.sql
@@ -1,12 +1,12 @@
 INSERT INTO wines
     (wine_name, winery, varietal, vintage, rating, notes, user_id)
     VALUES
-    ('Justin Cabernet', 'Justin', 'Cabernet Sauvignon', '2014', '5', 'Great cab!'),
-    ('Rombauer Chardonnay', 'Rombauer', 'Chardonnay', '2015', '4', 'My favorite'),
-    ('Veuve Clicquot Yellow Label Champagne Brut', 'Veuve Clicquot', 'Sparkling', null, 'Great champagne'),
-    ('Machete', 'Orin Swift', 'Red Blend', '2019', '5', 'Best red blend!'),
-    ('Kim Crawford Sauvignon Blanc', 'Kim Crawford', 'Sauvignon Blanc', '2020', '4', 'Great sauv blanc'),
-    ('Apothic Red', 'Apothic', 'Red Blenc', null, '3', 'Good for the price'),
-    ('The Prisoner Red Blend', 'The Prisoner Wine Company', 'Red Blend', '2019', '5', 'One of my favorites'),
-    ('La Marca Prosecco', 'La Marca', 'Sparkling', null, '4', 'Great sparkling for the price'),
-    ('Cote des Roses Rosé', 'Gérard Bertrand', 'Rosé', null, '4', 'Great rosé for the price');
+    ('Justin Cabernet', 'Justin', 'Cabernet Sauvignon', '2014', '5', 'Great cab!', 1),
+    ('Rombauer Chardonnay', 'Rombauer', 'Chardonnay', '2015', '4', 'My favorite', 1),
+    ('Veuve Clicquot Yellow Label Champagne Brut', 'Veuve Clicquot', 'Sparkling', null, '5', 'Great champagne', 1),
+    ('Machete', 'Orin Swift', 'Red Blend', '2019', '5', 'Best red blend!', 1),
+    ('Kim Crawford Sauvignon Blanc', 'Kim Crawford', 'Sauvignon Blanc', '2020', '4', 'Great sauv blanc', 1),
+    ('Apothic Red', 'Apothic', 'Red Blenc', null, '3', 'Good for the price', 1),
+    ('The Prisoner Red Blend', 'The Prisoner Wine Company', 'Red Blend', '2019', '5', 'One of my favorites', 1),
+    ('La Marca Prosecco', 'La Marca', 'Sparkling', null, '4', 'Great sparkling for the price', 1),
+    ('Cote des Roses Rosé', 'Gérard Bertrand', 'Rosé', null, '4', 'Great rosé for the price', 1);
